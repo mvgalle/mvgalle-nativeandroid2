7bbab3a6056705c52c411b16ffd92719a8639787
Matthew Gallegos <mvgalle@gmail.com>
2016-03-27 15:18:14 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+This is a port to meteor of http://nativedroid.godesign.ch/material/
+
+includes the followng packages:
+
+fortawesome:fontawesome
+komorebi:waves
+timmyg:wow
+mfpierre:chartist-js
+mizzao:jquery-ui
+less
+
+# Usage
+
+Just run `meteor add mvgalle:nativeandroid2` in your project, then use the follow the api usage on:
+
+    http://nativedroid.godesign.ch/material/
+
+
+# Issues
+
+If you encounter an issue while using this package, please CC @mvgalle
+
+
+# TODO
+
+* Create handle bar helpers to make it easier to add navigation and pages. For now look at nativedroid documentation.
---
+++ package.js
@@ -0,0 +1,40 @@
+Package.describe({
+  name: 'mvgalle:nativeandroid2',
+  version: '0.0.8',
+  // Brief, one-line summary of the package.
+  summary: 'port of http://nativedroid.godesign.ch/material/ to meteor',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.1');
+
+  // we bundle all font files, but the client will request only one of them via the CSS @font-face rule
+  api.addFiles(['jquery.mobile.min.js'], ["client"]);
+  api.addFiles(['nativeandroid2.js',
+    'css/flexboxgrid.min.css',
+    'css/googleRobotoDraft.css',
+    'css/googleRobotoItalic.css',
+    'css/jquery.mobile.min.css',
+    'css/material-design-iconic-font.min.css',
+    'css/nativedroid2.color.cyan.css',
+    'css/nativedroid2.css'
+      ], ["client"]);
+
+  api.addAssets(['fonts/Material-Design-Iconic-Font.eot',
+    'fonts/Material-Design-Iconic-Font.svg',
+    'fonts/Material-Design-Iconic-Font.ttf',
+    'fonts/Material-Design-Iconic-Font.woff'], ["client"]);
+});
+
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('mvgalle:nativeandroid2');
+  api.addFiles('nativeandroid2-tests.js');
+});
+
